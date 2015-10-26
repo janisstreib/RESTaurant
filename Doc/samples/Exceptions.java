e22745ce0f90db24b205f2c5dd31f7c7a8b04586
Janis Streib <janis@dogcraft.de>
2015-10-26 06:33:59 +0100

    ADD: Doc about exceptions

---
+++ Doc/samples/Exceptions.java
@@ -0,0 +1,16 @@
+public class Exceptions {
+	public void test1() {
+		try {
+			test2("hallo");
+		} catch (Exception e) {
+			// Den Stacktrace (Liste der Caller bis zum Auftreten der Exception)
+			e.printStackTrace();
+		}
+	}
+
+	public void test2(String hallo) throws Exception {
+		if (hallo != "hallo") {
+			throw new Exception("The String MUST be \"hallo\"!");
+		}
+	}
+}
