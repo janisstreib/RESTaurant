8c3baaa6dd0262ee92e987e3cada5550815fb3e2
Janis Streib <janis@dogcraft.de>
2015-11-01 13:17:54 +0100

    ADD: used new task

--- Zinsrechner/src/com/example/zinsrechner/MainActivity.java
+++ Zinsrechner/src/com/example/zinsrechner/MainActivity.java
@@ -7,32 +7,60 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private EditText kapital;
-	private EditText zinsSatz;
-	private EditText laufZeit;
+	private EditText zinssatz;
+	private EditText laufzeit;
+	private TextView jahre;
+	private double oldKapital = 0;
+	private double oldZinssatz = 0;
+	private int oldLaufzeit = 0;
+	private int summeJahre = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		kapital = (EditText) findViewById(R.id.kapital);
-		zinsSatz = (EditText) findViewById(R.id.zinssatz);
-		laufZeit = (EditText) findViewById(R.id.laufzeit);
+		zinssatz = (EditText) findViewById(R.id.zinssatz);
+		laufzeit = (EditText) findViewById(R.id.laufzeit);
+		jahre = (TextView) findViewById(R.id.jahre);
 		Button berechnen = (Button) findViewById(R.id.berechnen);
 		berechnen.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				try {
+					boolean isNew = false;
 					double k = Double.parseDouble(kapital.getText().toString());
+					if (k != oldKapital) {
+						isNew = true;
+					}
 					double z = Double
-							.parseDouble(zinsSatz.getText().toString());
-					int l = Integer.parseInt(laufZeit.getText().toString());
-					kapital.setText(Math.round(((k + k * (z / 100) * l)) * 100)
-							/ 100d + "");
+							.parseDouble(zinssatz.getText().toString());
+					if (z != oldZinssatz) {
+						isNew = true;
+						oldZinssatz = z;
+					}
+					int l = Integer.parseInt(laufzeit.getText().toString());
+					if (l != oldLaufzeit) {
+						isNew = true;
+						oldLaufzeit = l;
+					}
+					// Kapital linear ausrechnen und auf zwei Nchkommastellen
+					// runden
+					double newKapital = Math
+							.round(((k + k * (z / 100) * l)) * 100) / 100d;
+					oldKapital = newKapital;
+					kapital.setText(newKapital + "");
+					if (isNew) {
+						summeJahre = 0;
+					}
+					summeJahre += l;
+					jahre.setText(summeJahre + " Jahre");
 				} catch (NumberFormatException e) {
 					Toast.makeText(
 							getApplicationContext(),
